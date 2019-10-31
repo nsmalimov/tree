c24eb8aed5b0cf6fe7d5f5ef4c4b4b552b0d32af
Nurislam Alimov <nalimov@sardinasystems.com>
2019-10-31 23:52:33 +0300

    add banchmark test

--- test/walker_bench_test.go
+++ test/walker_bench_test.go
@@ -2,13 +2,38 @@ package test
 
 import (
 	"fmt"
+	"os"
 	"testing"
+
+	w "tree/internal/walker"
+
+	"github.com/sirupsen/logrus"
 )
 
+// mac os, 4 cors, 8gb, sync processing
+// BenchmarkSample-4           3135            384393 ns/op           28767 B/op        212 allocs/op
+
 func BenchmarkSample(b *testing.B) {
+	logger := logrus.New()
+
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		b.Fatalf("Error when try os.Getwd, err: %s", err)
+		return
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
 	for i := 0; i < b.N; i++ {
-		if x := fmt.Sprintf("%d", 42); x != "42" {
-			b.Fatalf("Unexpected string: %s", x)
-		}
+		walker := w.New(
+			logger,
+			fmt.Sprintf("%s/%s", currentDir, "/test_folders/1/www/theme"),
+			false,
+			false,
+		)
+
+		walker.StartSync()
 	}
 }
